799f333017104715e6ed7b45a5614002dc0fcb92
Geoff St. Pierre <serundeputy@gmail.com>
2021-01-17 16:25:00 -0500

    Distinguish ma_df and ct_df.

--- covid-data/modules/city_town.py
+++ covid-data/modules/city_town.py
@@ -1,4 +1,4 @@
-def city_town(df):
+def city_town(ma_df):
     towns = [
             "Chicopee",
             "Ludlow",
@@ -14,20 +14,20 @@ def city_town(df):
     for i in range(len(towns)):
         color_class = 'gray'
         if ((i + 1) % 3 == 0): print('\t<div class="row">\n')
-        if (df[df["City/Town"] == towns[i]]["Color"].tail(1).values[0] == 3):
+        if (ma_df[ma_df["City/Town"] == towns[i]]["Color"].tail(1).values[0] == 3):
             color_class = 'red'
-        elif (df[df["City/Town"] == towns[i]]["Color"].tail(1).values[0] == 2):
+        elif (ma_df[ma_df["City/Town"] == towns[i]]["Color"].tail(1).values[0] == 2):
             color_class = 'yellow'
-        elif (df[df["City/Town"] == towns[i]]["Color"].tail(1).values[0] == 1):
+        elif (ma_df[ma_df["City/Town"] == towns[i]]["Color"].tail(1).values[0] == 1):
             color_class = 'green'
-        elif (df[df["City/Town"] == towns[i]]["Color"].tail(1).values[0] == 0):
+        elif (ma_df[ma_df["City/Town"] == towns[i]]["Color"].tail(1).values[0] == 0):
             color_class = 'gray'
 
         print('\t\t<div class="', color_class, ' col-md-4">\n')
         print('<div class="town-block">')
         print('\t\t\t<span class="town">', towns[i], '</span>\n')
-        print('\t\t\t<br />Two Wk Case Count: ', df[df["City/Town"] == towns[i]]["Two Week Case Counts"].tail(1).values[0], '\n')
-        print('\t\t\t<br />Two Wk Positivity: ', round(df[df['City/Town'] == towns[i]]['Percent Positivity'].tail(1).values[0] * 100, 2), '%')
+        print('\t\t\t<br />Two Wk Case Count: ', ma_df[ma_df["City/Town"] == towns[i]]["Two Week Case Counts"].tail(1).values[0], '\n')
+        print('\t\t\t<br />Two Wk Positivity: ', round(ma_df[ma_df['City/Town'] == towns[i]]['Percent Positivity'].tail(1).values[0] * 100, 2), '%')
         print('</div>')
         print('\t\t</div>\n')
         if ((i + 1) % 3 == 0): print('\t</div>\n')
